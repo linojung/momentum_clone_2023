102c4159530acc3f289fdabae41b0d1c0c1f2c11
linojung <46038961+linojung@users.noreply.github.com>
2023-08-21 00:10:36 +0900

    test calculator
    learning the concept of return

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const calc = {
   plus: function (a, b) {
     return a + b;
   },
+  minus: function (a, b) {
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  power: function (a, b) {
+    return a ** b;
+  },
 };
 
-const result = calc.plus(1, 6);
+const plusResult = calc.plus(1, 6);
+const minusResult = calc.minus(plusResult, 10);
+const timesResult = calc.times(1, minusResult);
+const divideResult = calc.divide(timesResult, plusResult);
+const powerResult = calc.power(divideResult, minusResult);
 
-console.log(result);
+console.log(plusResult, minusResult, timesResult, divideResult, powerResult);
