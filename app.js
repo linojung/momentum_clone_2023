997bc2333dbf13034a172432c284fa7bbd0c6edf
linojung <46038961+linojung@users.noreply.github.com>
2023-08-21 00:40:00 +0900

    'return' execute the function
    so nothing works below the return-line

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-const calc = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
+const any = {
+  thing: function () {
+    return "hello";
+    console.log("bye bye");
   },
 };
-
-const plusResult = calc.plus(1, 6);
-const minusResult = calc.minus(plusResult, 10);
-const timesResult = calc.times(1, minusResult);
-const divideResult = calc.divide(timesResult, plusResult);
-const powerResult = calc.power(divideResult, minusResult);
-
-console.log(plusResult, minusResult, timesResult, divideResult, powerResult);
