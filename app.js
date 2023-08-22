1c27fe61283d105c966a57dfd0d2d8a084fad6f1
linojung <46038961+linojung@users.noreply.github.com>
2023-08-22 23:47:15 +0900

    Event Listener
    console.dir usage

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const grab = document.getElementById("title");
+const grab = document.querySelector(".box h1");
 
-grab.innerText = "Got You!";
+console.dir(grab);
+
+function handleGrabClick() {
+  grab.style.color = "blue";
+}
+
+function enterMouse() {
+  grab.innerText = "mouse is here";
+}
+
+function leaveMouse() {
+  grab.innerText = "mouse has left";
+}
+
+grab.addEventListener("click", handleGrabClick);
+grab.addEventListener("mouseenter", enterMouse);
+grab.addEventListener("mouseleave", leaveMouse);
