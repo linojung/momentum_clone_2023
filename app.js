4f3ce5c662e59badd8720c15a179f68a321d1fbd
linojung <46038961+linojung@users.noreply.github.com>
2023-08-26 23:31:13 +0900

    event.preventDefault()
    learned

---
+++ app.js
@@ -0,0 +1,11 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+  const username = loginInput.value;
+  console.log(username);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
