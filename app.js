4e94b48a1d675727630d6df7682494d8c9bec1b2
linojung <46038961+linojung@users.noreply.github.com>
2023-08-26 22:21:43 +0900

    ClassName method bug

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const h1 = document.querySelector(".box h1");
 h1.style.cursor = "pointer";
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "active";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
-  console.log(newColor);
 }
 
 h1.addEventListener("click", handleTitleClick);
