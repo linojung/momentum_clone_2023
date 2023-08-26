31e790e39371eee21dbeaa2445526fccaaef73ad
linojung <46038961+linojung@users.noreply.github.com>
2023-08-26 22:34:45 +0900

    Toggle is awesome!!!
    classList.toggle("classname")

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ h1.style.cursor = "pointer";
 
 function handleTitleClick() {
   const clickedClass = "active";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
+  h1.classList.toggle(clickedClass);
 }
 
 h1.addEventListener("click", handleTitleClick);
