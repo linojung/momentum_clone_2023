443974dfa2d6afe62a58efee356fc33461a074a1
linojung <46038961+linojung@users.noreply.github.com>
2023-08-28 15:06:38 +0900

    LOGIN to HI
    Should practice these lines

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+
+const USERNAME_KEY = "username";
 const HIDDEN_CLASSNAME = "hidden";
+function paintGreetings(username) {
+  greeting.innerText = `Hi ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+  paintGreetings(savedUsername);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
