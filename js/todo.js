fbf3eb4a7e80e27fd48a19ec514996e7bfcce590
linojung <46038961+linojung@users.noreply.github.com>
2023-08-30 00:48:19 +0900

    Update todo.js
    #7.2 Deleting To Dos
    event.target.parentNode

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event) {
+  console.log(event.target.parentNode.innerText);
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newToDo;
+  button.innerText = "x";
+  button.addEventListener("click", deleteToDo);
   console.log(li);
   toDoList.appendChild(li);
 }
