d212a4b78c986f28aee3b9af536c73a1914f1083
linojung <46038961+linojung@users.noreply.github.com>
2023-08-29 20:02:16 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -5,7 +5,16 @@ const images = [
   "bg4.jpg",
   "bg5.jpg",
   "bg6.jpg",
+  "bg7.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 console.log(chosenImage);
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+console.log(bgImage);
+
+document.body.appendChild(bgImage);
