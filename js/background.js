a874b5a30ae9212e9a6988fb7daa9446ab756d42
linojung <46038961+linojung@users.noreply.github.com>
2023-08-31 11:14:53 +0900

    blur image

--- js/background.js
+++ js/background.js
@@ -6,6 +6,14 @@ const images = [
   "bg5.jpg",
   "bg6.jpg",
   "bg7.jpg",
+  "bg8.jpg",
+  "bg9.jpg",
+  "bg10.jpg",
+  "bg11.jpg",
+  "bg12.jpg",
+  "bg13.jpg",
+  "bg14.jpg",
+  "bg15.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
@@ -15,3 +23,10 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
+
+const blurContainer = document.querySelector(".blur-img");
+const blurImg = document.createElement("img");
+
+blurImg.src = `img/${chosenImage}`;
+
+blurContainer.appendChild(blurImg);
