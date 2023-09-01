239234dc3de16a10b55e5187e6c34bb711fa0e1f
linojung <46038961+linojung@users.noreply.github.com>
2023-09-02 00:01:05 +0900

    making logo

--- js/clock.js
+++ js/clock.js
@@ -7,7 +7,8 @@ function getClock() {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clockHourMin.innerText = `${hours}:${minutes}`;
-  clockSec.innerText = `:${seconds}`;
+  clockSec.innerText = "";
+  //clockSec.innerText = `:${seconds}`;
 }
 
 getClock();
