d80b5ef7323bc7f00b2bd3ea8da2f1245b81785c
linojung <46038961+linojung@users.noreply.github.com>
2023-09-07 22:10:26 +0900

    retry todo

--- js/todo_failed.js
+++ js/todo_failed.js
@@ -4,8 +4,8 @@ const todoList = document.querySelector("#todo-list");
 const TODOS_KEY = "todos";
 const EMPTYTOOD_TEXT = document.querySelector("#empty-todo").innerText;
 let todos = [];
-const BOX_ICON_EMPTY = "fa-square";
-const BOX_ICON_CHECKED = "fa-check-square";
+const BOX_ICON_EMPTY = "img/checkbox-empty.png";
+const BOX_ICON_CHECKED = "img/checkbox-checked.png";
 const LABEL_CROSSED = "todo-crossed";
 const CHECK_KEY = "check";
 let checkedTodos = [];
@@ -13,8 +13,7 @@ let checkedTodos = [];
 function saveTodos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
-
-function saveChecked() {
+function saveCheckedTodos() {
   localStorage.setItem(CHECK_KEY, JSON.stringify(checkedTodos));
 }
 
@@ -34,32 +33,46 @@ function deleteTodo(event) {
   if (todosLength === 0) {
     addEmptyTodo();
   }
+  checkedTodos = checkedTodos.filter(
+    (item) => item.checkedId !== parseInt(li.id)
+  );
+  saveCheckedTodos();
+}
+
+function checkLabel(checkBox) {
+  const id = checkBox.parentNode.id;
+  const checkedId = {
+    checkedId: JSON.parse(id),
+  };
+  const label = checkBox.parentNode.querySelector("label");
+  const fakeBox = checkBox.parentNode.querySelector("img");
+
+  if (checkBox.checked) {
+    fakeBox.setAttribute("src", BOX_ICON_CHECKED);
+    label.classList.add(LABEL_CROSSED);
+  } else {
+    fakeBox.setAttribute("src", BOX_ICON_EMPTY);
+    label.classList.remove(LABEL_CROSSED);
+  }
+}
+
+function checkedPushPop(checkBox) {
+  const id = checkBox.parentNode.id;
+  const checkedId = {
+    checkedId: JSON.parse(id),
+  };
+  if (checkBox.checked) {
+    checkedTodos.push(checkedId);
+  } else {
+    checkedTodos.pop(checkedId);
+  }
+  saveCheckedTodos();
 }
 
 function handleCheckBoxChange(event) {
-  const ID = event.target.parentNode.id;
-  const newCheckedObject = {
-    id: ID,
-  };
   const checkBox = event.target;
-  const label = checkBox.parentNode.querySelector("label");
-  const fakeBox = checkBox.parentNode.querySelector("i");
-  if (checkBox.checked) {
-    fakeBox.classList.remove(BOX_ICON_EMPTY);
-    fakeBox.classList.add(BOX_ICON_CHECKED);
-    label.classList.add(LABEL_CROSSED);
-    checkedTodos.push(newCheckedObject);
-    console.log(checkedTodos);
-    // checkedTodos = newCheckedObject;
-  } else {
-    fakeBox.classList.remove(BOX_ICON_CHECKED);
-    fakeBox.classList.add(BOX_ICON_EMPTY);
-    label.classList.remove(LABEL_CROSSED);
-    checkedTodos.pop(newCheckedObject);
-    // checkedTodos = newCheckedObject;
-    console.log(checkedTodos);
-  }
-  saveChecked();
+  checkedPushPop(checkBox);
+  checkLabel(checkBox);
 }
 
 function paintTodo(newTodo) {
@@ -67,8 +80,8 @@ function paintTodo(newTodo) {
   const checkBox = document.createElement("input");
   checkBox.setAttribute("type", "checkbox");
   const label = document.createElement("label");
-  const fake = document.createElement("i");
-  fake.classList.add("fas", BOX_ICON_EMPTY);
+  const fake = document.createElement("img");
+  fake.setAttribute("src", BOX_ICON_EMPTY);
   const span = document.createElement("span");
   const button = document.createElement("button");
   const emptyLi = document.querySelector("#empty-todo");
@@ -106,13 +119,46 @@ function handleTodoSubmit(event) {
   saveTodos();
 }
 
+function checkHasId(checkedTodo) {
+  let id = checkedTodo.checkedId;
+  id = JSON.stringify(id);
+  const liHasId = document.getElementById(id);
+  const checkInput = liHasId.querySelector("input[type='checkbox']");
+  checkInput.checked = true;
+  checkLabel(checkInput);
+}
 //on page load
 
 todoForm.addEventListener("submit", handleTodoSubmit);
 
 const savedTodos = localStorage.getItem(TODOS_KEY);
+const savedCheckedTodos = localStorage.getItem(CHECK_KEY);
+
 if (savedTodos !== null) {
   const parsedTodos = JSON.parse(savedTodos);
   todos = parsedTodos;
   parsedTodos.forEach(paintTodo);
+  //
 }
+if (savedCheckedTodos !== null) {
+  const parsedCheckedTodos = JSON.parse(savedCheckedTodos);
+  checkedTodos = parsedCheckedTodos;
+  parsedCheckedTodos.forEach(checkHasId);
+  // todos = todos.filter((item) => item.id !== parseInt(li.id));
+}
+
+//check all todo
+const todoAll = document.querySelector("#todo-all");
+const todoAllInput = todoAll.querySelector("input[type='checkbox']");
+const todoListInputs = todoList.querySelectorAll("input[type='checkbox']");
+
+function handleTodoAllChange() {
+  const fakeBox = todoAll.querySelector("img");
+  if (todoAllInput.checked) {
+    fakeBox.setAttribute("src", BOX_ICON_CHECKED);
+  } else {
+    fakeBox.setAttribute("src", BOX_ICON_EMPTY);
+  }
+}
+
+todoAllInput.addEventListener("change", handleTodoAllChange);
