56ac28b32186e0702f958b624d857849edfee1df
linojung <46038961+linojung@users.noreply.github.com>
2023-09-08 01:40:36 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -4,18 +4,10 @@ const todoList = document.querySelector("#todo-list");
 const TODOS_KEY = "todos";
 const EMPTYTOOD_TEXT = document.querySelector("#empty-todo").innerText;
 let todos = [];
-const BOX_ICON_EMPTY = "fa-square";
-const BOX_ICON_CHECKED = "fa-check-square";
-const LABEL_CROSSED = "todo-crossed";
-const CHECK_KEY = "check";
-let checkedTodos = [];
+let checks = [];
 
 function saveTodos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
-}
-
-function saveChecked() {
-  localStorage.setItem(CHECK_KEY, JSON.stringify(checkedTodos));
 }
 
 function addEmptyTodo() {
@@ -36,61 +28,29 @@ function deleteTodo(event) {
   }
 }
 
-function handleCheckBoxChange(event) {
-  const ID = event.target.parentNode.id;
-  const newCheckedObject = {
-    id: ID,
-  };
-  const checkBox = event.target;
-  const label = checkBox.parentNode.querySelector("label");
-  const fakeBox = checkBox.parentNode.querySelector("i");
-  if (checkBox.checked) {
-    fakeBox.classList.remove(BOX_ICON_EMPTY);
-    fakeBox.classList.add(BOX_ICON_CHECKED);
-    label.classList.add(LABEL_CROSSED);
-    checkedTodos.push(newCheckedObject);
-    console.log(checkedTodos);
-    // checkedTodos = newCheckedObject;
-  } else {
-    fakeBox.classList.remove(BOX_ICON_CHECKED);
-    fakeBox.classList.add(BOX_ICON_EMPTY);
-    label.classList.remove(LABEL_CROSSED);
-    checkedTodos.pop(newCheckedObject);
-    // checkedTodos = newCheckedObject;
-    console.log(checkedTodos);
-  }
-  saveChecked();
-}
-
 function paintTodo(newTodo) {
   const li = document.createElement("li");
-  const checkBox = document.createElement("input");
-  checkBox.setAttribute("type", "checkbox");
-  const label = document.createElement("label");
-  const fake = document.createElement("i");
-  fake.classList.add("fas", BOX_ICON_EMPTY);
   const span = document.createElement("span");
   const button = document.createElement("button");
   const emptyLi = document.querySelector("#empty-todo");
+  const check = document.createElement("input");
+  const label = document.createElement("label");
+  const img = document.createElement("img");
   if (emptyLi !== null) {
     emptyLi.remove();
   }
 
   li.id = newTodo.id;
-  const CHECKBOX_ID = `check${newTodo.id}`;
-  checkBox.id = CHECKBOX_ID;
-  label.setAttribute("for", CHECKBOX_ID);
   span.innerText = newTodo.text;
   button.innerText = "x";
+  check.setAttribute("type", "checkbox");
+  check.id = `check${li.id}`;
   todoList.appendChild(li);
-  li.appendChild(checkBox);
-  li.appendChild(label);
-  label.appendChild(fake);
-  label.appendChild(span);
+  li.appendChild(check);
+  li.appendChild(span);
   li.appendChild(button);
   //events
   button.addEventListener("click", deleteTodo);
-  checkBox.addEventListener("change", handleCheckBoxChange);
 }
 
 function handleTodoSubmit(event) {
