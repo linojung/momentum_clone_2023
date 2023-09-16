2ceccd7e050302712ddc028941df039ab1181cd9
linojung <46038961+linojung@users.noreply.github.com>
2023-09-17 02:15:22 +0900

    overall

--- js/background.js
+++ js/background.js
@@ -1,19 +1,19 @@
 const images = [
   "bg1.jpg",
-  "bg2.jpg",
+  // "bg2.jpg",
   "bg3.jpg",
   "bg4.jpg",
   "bg5.jpg",
   "bg6.jpg",
-  "bg7.jpg",
-  "bg8.jpg",
+  // "bg7.jpg",
+  // "bg8.jpg",
   "bg9.jpg",
-  "bg10.jpg",
+  // "bg10.jpg",
   "bg11.jpg",
   "bg12.jpg",
-  "bg13.jpg",
+  // "bg13.jpg",
   "bg14.jpg",
-  "bg15.jpg",
+  // "bg15.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
