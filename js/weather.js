72786638785df739f5170b505b397a8ba6e5339d
linojung <46038961+linojung@users.noreply.github.com>
2023-09-17 00:49:02 +0900

    weather icon

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,31 @@
-// const API_KEY = "b30cb7bb6ffb53a3b485a06e8874a96b";
+const API_KEY = "b30cb7bb6ffb53a3b485a06e8874a96b";
 
-// function onGeoOk(position) {
-//   const lat = position.coords.latitude;
-//   const lon = position.coords.longitude;
-//   console.log("you live in", lat, lon);
-//   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const city = document.querySelector(".geo__city");
-//       const weather = document.querySelector(".geo__weather");
-//       const temp = document.querySelector(".geo__temp");
-//       const weatherData = data.weather[0].main;
-//       city.innerText = data.name;
-//       temp.innerText = `${data.main.temp}°c (${weatherData})`;
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("you live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector(".geo__city");
+      const weather = document.querySelector(".geo__weather");
+      const temp = document.querySelector(".geo__temp");
+      const weatherData = data.weather[0].main;
+      city.innerText = data.name;
+      temp.innerText = `${data.main.temp}°c`;
+      const icon = document.createElement("img");
+      weather.appendChild(icon);
+      console.log(weatherData);
+      if (weatherData === "Rain") {
+        icon.setAttribute("src", "img/wheather/rain.png");
+        console.log("raining");
+      }
+    });
+}
 
-//     });
-// }
+function onGeoError() {
+  alert("Can't find where you live");
+}
 
-// function onGeoError() {
-//   alert("Can't find where you live");
-// }
-
-// navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
