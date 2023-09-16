a00def0b18f856b1617623321de5791a4b63b79a
linojung <46038961+linojung@users.noreply.github.com>
2023-09-17 01:32:19 +0900

    weather done

--- js/weather.js
+++ js/weather.js
@@ -12,14 +12,27 @@ function onGeoOk(position) {
       const weather = document.querySelector(".geo__weather");
       const temp = document.querySelector(".geo__temp");
       const weatherData = data.weather[0].main;
+      const weatherDescription = data.weather[0].description;
+      //   const weatherData = data.weather[0].main;
       city.innerText = data.name;
       temp.innerText = `${data.main.temp}°c`;
       const icon = document.createElement("img");
-      weather.appendChild(icon);
-      console.log(weatherData);
+      weather.innerText = weatherDescription;
+      weather.append(icon);
       if (weatherData === "Rain") {
-        icon.setAttribute("src", "img/wheather/rain.png");
-        console.log("raining");
+        icon.setAttribute("src", "img/weather/rain.png");
+      } else if (weatherData === "Thunderstorm") {
+        icon.setAttribute("src", "img/weather/thunderstorm.png");
+      } else if (weatherData === "Drizzle") {
+        icon.setAttribute("src", "img/weather/drizzle.png");
+      } else if (weatherData === "Snow") {
+        icon.setAttribute("src", "img/weather/snow.png");
+      } else if (weatherData === "Clear") {
+        icon.setAttribute("src", "img/weather/clear.png");
+      } else if (weatherData === "Clouds") {
+        icon.setAttribute("src", "img/weather/cloud.png");
+      } else {
+        icon.setAttribute("src", "img/weather/atmosphere.png");
       }
     });
 }
